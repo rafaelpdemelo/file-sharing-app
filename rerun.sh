4aef1499e0e0235b831a460a4e211119d447a038
Rafael Pereira <rafael.pereira@liferay.com>
2023-07-28 21:42:16 -0300

    Edit rerun.sh script to user change the username in script

--- rerun.sh
+++ rerun.sh
@@ -2,11 +2,11 @@
 
 # Navega para o diretório /backend e constrói a imagem Docker para o servidor Node.js
 cd backend/
-docker buildx build -t rafaelpdemelo/node-app:latest -f node.Dockerfile .
+docker buildx build -t [username]/node-app:latest -f node.Dockerfile .
 
 # Navega para o diretório /frontend e constrói a imagem Docker para o aplicativo React
 cd ../frontend/
-docker buildx build -t rafaelpdemelo/react-app:latest -f react.Dockerfile .
+docker buildx build -t [username]/react-app:latest -f react.Dockerfile .
 
 # Para todos os contêineres Docker em execução
 docker stop $(docker ps -aq)
@@ -15,7 +15,7 @@ docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 
 # Inicia um contêiner Docker para o aplicativo React na porta 80
-docker run -p 80:80 -d rafaelpdemelo/react-app:latest
+docker run -p 80:80 -d [username]/react-app:latest
 
 # Inicia um contêiner Docker para o servidor Node.js na porta 3001
-docker run -p 3001:3001 -d rafaelpdemelo/node-app:latest
+docker run -p 3001:3001 -d [username]/node-app:latest
